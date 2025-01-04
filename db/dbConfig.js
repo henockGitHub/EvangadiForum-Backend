4dc7deaeb45262dfc545f5e34f40893599410c5d
HeniMan <henock2000@gmail.com>
2025-01-04 12:57:44 +0100

    database deployment

--- db/dbConfig.js
+++ db/dbConfig.js
@@ -1,20 +1,25 @@
+const mysql2 = require("mysql2");
+const DB_HOST = process.env.DB_HOST;
+const DB_USER = process.env.DB_USER;
+const DB_PASSWORD = process.env.DB_PASSWORD;
+const DB_NAME = process.env.DB_NAME;
+const DB_PORT = process.env.DB_PORT;
 
-const mysql2=require('mysql2')
-const dbconnection =mysql2.createPool({
-  user: process.env.USER,
-  database: process.env.DATABASE,
-  host:"localhost",
-  password:process.env.PASSWORD,
-  connectionLimit:10  
-})
+const dbconnection = mysql2.createPool({
+  host: DB_HOST,
+  user: DB_USER,
+  password: DB_PASSWORD,
+  database: DB_NAME,
+  port: DB_PORT,
+  waitForConnections: true,
+  connectionLimit: 10,
+});
 
-dbconnection.execute("select 'test'",(err,result)=>{
-    if(err)
-    {
-        console.log(err.message)
-    }
-    else{
-        console.log(result)
-    }
-    })
-module.exports=dbconnection.promise()
+dbconnection.execute("select 'test'", (err, result) => {
+  if (err) {
+    console.log(err.message);
+  } else {
+    console.log(result);
+  }
+});
+module.exports = dbconnection.promise();
