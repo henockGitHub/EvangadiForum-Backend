d2e9967d48dbbe6c147dc817657923c33e340486
HeniMan <henock2000@gmail.com>
2025-01-04 13:57:11 +0100

    fix bug

--- Controller/userController.js
+++ Controller/userController.js
@@ -54,7 +54,9 @@ async function login(req,res){
             }  
         const username=user[0].username
         const userid=user[0].userId;
-        const token=jwt.sign({username,userid}, process.env.JWT_SECRET, {expiresIn:"1d"})
+        const token = jwt.sign({ username, userid }, process.env.jwt_secret, {
+          expiresIn: "1d",
+        });
 
          return res.status(StatusCodes.OK).json({msg:"user login successful", token})
         
